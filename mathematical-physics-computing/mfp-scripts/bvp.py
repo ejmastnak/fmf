4e3a85d62d96bd0998c1527415d067963770920b
Elijan Mastnak <ejmastnak@gmail.com>
2020-11-29 13:16:47 +0100

    add bvp scripts

---
+++ mathematical-physics-computing/mfp-scripts/bvp.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib
+from matplotlib import pyplot as plt
+import os
+import re
+import time
+from numerical_methods_odes import *
+
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+color_blue = "#244d90"  # darker teal / blue
+color_teal = "#3997bf"  # lighter teal/blue
+
+color_orange_dark = "#91331f"  # dark orange
+color_orange_mid = "#e1692e"  # mid orange
+color_orange_light = "#f5c450"  # light orange
+
+color_gray_ref = "#AAAAAA"  # light gray for reference quantities
+
+# data_dir = "data/"
+data_dir = "/Users/ejmastnak/Documents/Media/academics/fmf-media-winter-3/mafiprak/bvp/"
+error_dir = data_dir + "error-step/"
+time_dir = data_dir + "times/"
+step_global_error_dir = data_dir + "stepsize-vs-global-error/"
+step_local_error_dir = data_dir + "stepsize-vs-local-error/"
+
+figure_dir = "../8-boundary-value-problem/figures/"
+save_figures = True
+
+
+# -----------------------------------------------------------------------------
+# START
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# END
+# -----------------------------------------------------------------------------
+
+
+def practice():
+    # x = np.linspace(0, 1, 10)
+    # y = np.linspace(10, 12.2342, 10)
+    # data = np.column_stack([x, y])
+    # print(data)
+    # data = np.column_stack([x, y, x])
+    # filename = data_dir + "test.csv"
+    # header = "Max error:,{}\nTime [h], Temp [C], Abs Error [C]".format(np.max(y))
+    # np.savetxt(filename, data, delimiter=',',  header=header)
+
+    a = (1, 2, 3, 4)
+    b = np.asarray(a)
+    print(b)
+
+
+def run():
+    practice()
+
+
+run()
---
+++ mathematical-physics-computing/mfp-scripts/bvp_methods.py
@@ -0,0 +1,235 @@
+import math
+import numpy as np
+from matplotlib import pyplot as plt
+
+"""A variety of methods to solve ODE boundary value problems.
+
+AUTHOR:
+    Jonathan Senning <jonathan.senning@gordon.edu>
+    Gordon College
+    Based Octave functions written in the spring of 1999
+    Python version: October-November 2008
+"""
+
+import numpy
+
+#-----------------------------------------------------------------------------
+
+def shoot( f, a, b, z1, z2, t, tol ):
+    """Implements the shooting method to solve second order BVPs
+
+    USAGE:
+        y = shoot(f, a, b, z1, z2, t, tol)
+
+    INPUT:
+        f     - function dy/dt = f(y,t).  Since we are solving a second-
+                order boundary-value problem that has been transformed
+                into a first order system, this function should return a
+                1x2 array with the first entry equal to y and the second
+                entry equal to y'.
+        a     - solution value at the left boundary: a = y(t[0]).
+        b     - solution value at the right boundary: b = y(t[n-1]).
+        z1    - first initial estimate of y'(t[0]).
+        z1    - second initial estimate of y'(t[0]).
+        t     - array of n time values to determine y at.
+        tol   - allowable tolerance on right boundary: | b - y[n-1] | < tol
+
+    OUTPUT:
+        y     - array of solution function values corresponding to the
+                values in the supplied array t.
+
+    NOTE:
+        This function assumes that the second order BVP has been converted to
+        a first order system of two equations.  The secant method is used to
+        refine the initial values of y' used for the initial value problems.
+    """
+
+    from diffeq import rk4
+
+    max_iter = 25   # Maximum number of shooting iterations
+
+    n = len( t )    # Determine the size of the arrays we will generate
+
+    # Compute solution to first initial value problem (IVP) with y'(a) = z1.
+    # Because we are using the secant method to refine our estimates of z =
+    # y', we don't really need all the solution of the IVP, just the last
+    # point of it -- this is saved in w1.
+
+    y = rk4( f, [a,z1], t )
+    w1 = y[n-1,0]
+
+    # print ("%2d: z = %10.3e, error = %10.3e" % ( 0, z1, b - w1 ))
+
+    # Begin the main loop.  We will compute the solution of a second IVP and
+    # then use the both solutions to refine our estimate of y'(a).  This
+    # second solution then replaces the first and a new "second" solution is
+    # generated.  This process continues until we either solve the problem to
+    # within the specified tolerance or we exceed the maximum number of
+    # allowable iterations.
+
+    for i in range( max_iter ):
+
+        # Solve second initial value problem, using y'(a) = z2.  We need to
+        # retain the entire solution vector y since if y(t(n)) is close enough
+        # to b for us to stop then the first column of y becomes our solution
+        # vector.
+
+        y = rk4( f, [a,z2], t )
+        w2 = y[n-1,0]
+
+        # print ("%2d: z = %10.3e, error = %10.3e" % ( i+1, z2, b - w2 ))
+
+        # Check to see if we are done...
+
+        if abs( b - w2 ) < tol:
+            break
+
+        # Compute the new approximations to the initial value of the first
+        # derivative.  We compute z2 using a linear fit through (z1,w1) and
+        # (z2,w2) where w1 and w2 are the estimates at t=b of the initial
+        # value problems solved above with y1'(a) = z1 and y2'(a) = z2.  The
+        # new value for z1 is the old value of z2.
+
+        #z1, z2 = ( z2, z1 + ( z2 - z1 ) / ( w2 - w1 ) * ( b - w1 ) )
+        z1, z2 = ( z2, z2 + ( z2 - z1 ) / ( w2 - w1 ) * ( b - w2 ) )
+        w1 = w2
+
+    # All done.  Check to see if we really solved the problem, and then return
+    # the solution.
+
+    if abs( b - w2 ) >= tol:
+        print ("\a**** ERROR ****")
+        print ("Maximum number of iterations (%d) exceeded" % max_iter)
+        print ("Returned values may not have desired accuracy")
+        print ("Error estimate of returned solution is %e" % ( b - w2 ))
+
+    return y[:,0]
+
+#-----------------------------------------------------------------------------
+
+def fd( u, v, w, t, a, b ):
+    """Implements the finite difference  method to solve linear second order BVPs
+
+    Compute finite difference solution to the BVP
+
+        x'' = u(t) + v(t) x + w(t) x'
+        x(t[0]) = a, x(t[n-1]) = b
+
+    t should be passed in as an n element array.   u, v, and w should be
+    either n element arrays corresponding to u(t), v(t) and w(t) or
+    scalars, in which case an n element array with the given value is
+    generated for each of them.
+
+    USAGE:
+        x = fd(u, v, w, t, a, b)
+
+    INPUT:
+        u,v,w - arrays containing u(t), v(t), and w(t) values.  May be
+                specified as Python lists, NumPy arrays, or scalars.  In
+                each case they are converted to NumPy arrays.
+        t     - array of n time values to determine x at
+        a     - solution value at the left boundary: a = x(t[0])
+        b     - solution value at the right boundary: b = x(t[n-1])
+
+    OUTPUT:
+        x     - array of solution function values corresponding to the
+                values in the supplied array t.
+    """
+
+    # Get the dimension of t and make sure that t is an n-element vector
+
+    if type( t ) != numpy.ndarray:
+        if type( t ) == list:
+            t = numpy.array( t )
+        else:
+            t = numpy.array( [ float( t ) ] )
+
+    n = len( t )
+
+    # Make sure that u, v, and w are either scalars or n-element vectors.
+    # If they are scalars then we create vectors with the scalar value in
+    # each position.
+
+    if type( u ) == int or type( u ) == float:
+        u = numpy.array( [ float( u ) ] * n )
+
+    if type( v ) == int or type( v ) == float:
+        v = numpy.array( [ float( v ) ] * n )
+
+    if type( w ) == int or type( w ) == float:
+        w = numpy.array( [ float( w ) ] * n )
+
+    # Compute the stepsize.  It is assumed that all elements in t are
+    # equally spaced.
+
+    h = t[1] - t[0];
+
+    # Construct tridiagonal system; boundary conditions appear as first and
+    # last equations in system.
+
+    A = -( 1.0 + w[1:n] * h / 2.0 )
+    A[-1] = 0.0
+
+    C = -( 1.0 - w[0:n-1] * h / 2.0 )
+    C[0] = 0.0
+
+    D = 2.0 + h * h * v
+    D[0] = D[n-1] = 1.0
+
+    B = - h * h * u
+    B[0] = a
+    B[n-1] = b
+
+    # Solve tridiagonal system
+
+    for i in range( 1, n ):
+        xmult = A[i-1] / D[i-1]
+        D[i] = D[i] - xmult * C[i-1]
+        B[i] = B[i] - xmult * B[i-1]
+
+    x = numpy.zeros( n )
+    x[n-1] = B[n-1] / D[n-1]
+
+    for i in range( n - 2, -1, -1 ):
+        x[i] = ( B[i] - C[i] * x[i+1] ) / D[i]
+
+    return x
+
+
+def get_d2xdt(y, t):
+    """ 2nd order ODE y'' = y + 4e^t """
+    return y + 4*np.exp(t)
+
+
+def f(y, t):
+    """ Re-written version of above get_d2xdt """
+    return_y = np.zeros(np.shape(y))
+    return_y[0] = y[1]
+    return_y[1] = y[0] + 4*np.exp(t)
+    return return_y
+
+
+def get_y_analytic(t):
+    """ Analytic solution to y'' = y + 4e^t with y(0)=1 and y(1/2) = 2e^(1/2)"""
+    return np.exp(t)*(1 + 2*t)
+
+
+def yes():
+    """ Solves y'' = y + 4exp(t), y(0)=1, y(1/2) = 2exp(1/2) using both the
+         finite difference method and the shooting method. """
+
+    a, b = 0.0, 0.5
+    n_points = 10
+    t1 = np.linspace(a, b, n_points)
+    t2 = np.linspace(a, b, 200)
+    y_analytic = get_y_analytic(t2)
+    y_fd = fd(4*np.exp(t1), 1, 0, t1, 1, 2*np.exp(0.5))
+    y_s = shoot(f, np.exp(a), 2*np.exp(b), 3.0, 4.0, t1, 1e-5)
+    plt.plot(t2, y_analytic, label="analytic solution")
+    plt.plot(t1, y_fd, ls='--', marker='o', label="finite difference method")
+    plt.plot(t1, y_s, ls='--', marker='o', label="shooting method")
+    plt.legend()
+    plt.show()
+
+
+yes()
