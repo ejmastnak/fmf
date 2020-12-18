ac5d64d4914903a19a72325329f08340d11085b4
Elijan Mastnak <ejmastnak@gmail.com>
2020-12-18 16:38:55 +0100

    create galerkin.py

---
+++ mathematical-physics-computing/mfp-scripts/galerkin.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rc('axes', labelsize=12)    # fontsize of the x and y labels
+plt.rc('axes', titlesize=16)    # fontsize of titles
+
+color_blue = "#244d90"  # darker teal / blue
+color_teal = "#3997bf"  # lighter teal/blue
+
+color_orange_dark = "#91331f"  # dark orange
+color_orange_mid = "#e1692e"  # mid orange
+color_orange_light = "#f5c450"  # light orange
+
+color_gray_ref = "#AAAAAA"  # light gray for reference quantities
+
+data_dir = "/Users/ejmastnak/Documents/Dropbox/academics/fmf-local/fmf-winter-3/mafiprak/11-galerkin/data/"
+figure_dir = "/Users/ejmastnak/Documents/Dropbox/academics/fmf-local/fmf-winter-3/mafiprak/11-galerkin/figures/"
+
+save_figures = True
+# usetex = True  # turn on to use Latex to render text
+usetex = False  # turn off to plot faster
+
+
+# -----------------------------------------------------------------------------
+# START ANALYSIS FUNCTIONS
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
+# END ANALYSIS FUNCTIONS
+# -----------------------------------------------------------------------------
+
+
+def practice():
+    N = 10
+    a = np.zeros((N, 1))
+    b = a[:, 0]
+    print(b)
+
+
+if __name__ == "__main__":
+    practice()
